a07459b0bea6a648b55d22143c246be6bfe1bc6e
chaitanyaGosavi <97080993+chaitanyaGosavi@users.noreply.github.com>
2022-05-02 00:43:18 +0530

    Added files

---
+++ enquiry.php
@@ -0,0 +1,20 @@
+<?php
+$con = mysqli_connect("localhost:3325", "root", "", "merabharat");
+
+$userName = $_POST['userName'];
+$userEmail = $_POST['userEmail'];
+$userPhone = $_POST['userPhone'];
+$destOption = $_POST['destOption'];
+
+$sql_add = "INSERT INTO `enquiry` (`name`, `email`, `phone`, `destination`, `id`) VALUES ('$userName', '$userEmail', '$userPhone', '$destOption', 0)";
+
+$rs = mysqli_query($con, $sql_add);
+
+if($rs)
+{
+	header("Refresh:0; url = http://localhost/meraBharat/home.html");
+}
+else {
+	echo("error". mysqli_error($con));
+}
+?>
